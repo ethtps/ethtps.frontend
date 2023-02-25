26f62632a86e1ca909a65b858e7b8bdfe28fec40
Mister_Eth <89896648+Mister-Eth@users.noreply.github.com>
2023-02-25 22:22:59 +0200

    Local dep loading

--- ethtps.frontend.v2/next.config.js
+++ ethtps.frontend.v2/next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+// see https://github.com/martpie/next-transpile-modules#readme
+const withTM = require("next-transpile-modules")(["ethtps.pages"]);
 
-module.exports = nextConfig
+module.exports = withTM({
+  reactStrictMode: true,
+  env: {},
+});
--- ethtps.pages/src/TestPage.tsx
+++ ethtps.pages/src/TestPage.tsx
@@ -1,10 +1,6 @@
 import { Fragment } from "react";
 
 export function TestPage() {
-  return (
-    <Fragment>
-      <h1>Test Page</h1>
-    </Fragment>
-  );
+  return <h1>Test Page</h1>;
 }
 export {};
