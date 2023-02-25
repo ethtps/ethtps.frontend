f6deba0349c077bfca2143edb8ab04ef8ec6d48d
Mister_Eth <89896648+Mister-Eth@users.noreply.github.com>
2023-02-25 15:20:11 +0200

    Set up SSR

--- ethtps.frontend.v2/pages/_app.tsx
+++ ethtps.frontend.v2/pages/_app.tsx
@@ -1,30 +1,20 @@
+// _app.tsx
 import { AppProps } from "next/app";
-import Head from "next/head";
 import { MantineProvider } from "@mantine/core";
+import { rtlCache } from "../rtl-cache";
 
 export default function App(props: AppProps) {
   const { Component, pageProps } = props;
-
   return (
-    <>
-      <Head>
-        <title>Page title</title>
-        <meta
-          name="viewport"
-          content="minimum-scale=1, initial-scale=1, width=device-width"
-        />
-      </Head>
-
+    <div dir="rtl">
       <MantineProvider
+        theme={{ dir: "rtl" }}
         withGlobalStyles
         withNormalizeCSS
-        theme={{
-          /** Put your mantine theme override here */
-          colorScheme: "light",
-        }}
+        emotionCache={rtlCache}
       >
         <Component {...pageProps} />
       </MantineProvider>
-    </>
+    </div>
   );
 }
