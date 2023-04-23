f6a1193bc30f31551b967671bb6fa5d21027f05d
Mister_Eth <89896648+Mister-Eth@users.noreply.github.com>
2023-04-24 02:22:53 +0300

    Update HeatmapTab.tsx

--- src/components/partials/provider-page/analysis/breakdown/transaction-type/HeatmapTab.tsx
+++ src/components/partials/provider-page/analysis/breakdown/transaction-type/HeatmapTab.tsx
@@ -2,10 +2,11 @@
 import styles from '../../../../../../styles/app.module.scss'
 import { ProviderResponseModel } from "@/api-client"
 import { Heatmap } from "../.."
-import { Text, Group, Modal, Paper, Space, Title, Center, Select } from "@mantine/core"
+import { Image, Text, Group, Modal, Paper, Space, Title, Center, Select } from "@mantine/core"
 import { ChartActions, SocialButtons, useViewportRatio } from "@/components"
 import { useDisclosure } from "@mantine/hooks"
 import { useCallback, useState } from "react"
+import { conditionalRender } from '@/services'
 
 interface IHeatmapTabProps {
     provider: ProviderResponseModel
@@ -34,6 +35,36 @@ export function HeatmapTab(props: Partial<IHeatmapTabProps>) {
             <Paper sx={{
                 padding: '1rem',
             }}>
+                <Center>
+                    <Group sx={{
+                        marginLeft: '2rem'
+                    }}>
+                        <Text className={'inline'} sx={{
+                            marginRight: '0.1rem',
+                            marginLeft: '0.1rem'
+                        }}>Average {props.provider?.name}</Text>
+                        <Select
+                            onChange={(e) => setBreakdown(e as Breakdowns)}
+                            className={'inline'}
+                            defaultValue={Breakdowns.tps}
+                            data={Object.values(Breakdowns).map(x => {
+                                return { label: x, value: x }
+                            })}
+                        />
+                        <Text className={'inline'} sx={{
+                            marginRight: '0.1rem',
+                            marginLeft: '0.1rem'
+                        }}>grouped by</Text>
+                        <Select className={'inline'}
+                            width={'auto'}
+                            onChange={(e) => setBreakdownPeriod(e as BreakdownPeriods)}
+                            defaultValue={BreakdownPeriods.year}
+                            data={Object.values(BreakdownPeriods).map(x => {
+                                return { label: x, value: x }
+                            })}
+                        />
+                    </Group>
+                </Center>
                 <Heatmap
                     from={`${year - minusYearsShown}-01-01`}
                     to={`${year + plusYearsShown}-12-31`}
@@ -41,44 +72,22 @@ export function HeatmapTab(props: Partial<IHeatmapTabProps>) {
                     interactive={true} />
             </Paper>
         </>
-    }, [props.provider, year])
+    }, [props.provider, year, modelOpened, ratio])
+
     return <>
         <Modal
-            fullScreen={ratio < 1.5}
+            fullScreen={ratio < 1}
             opened={modelOpened} size="calc(100vw - 1rem)" onClose={close} title={
-                <Text className={styles.logoish}>ETHTPS.info</Text>
+                <Text style={{ verticalAlign: "middle" }} className={styles.logoish}>ETHTPS.info</Text>
             } centered >
             <Group position='center'>
-                <Title order={3}>Average TPS by day </Title>
                 <SocialButtons />
             </Group>
             <Center>
                 {getHeatmap()}
             </Center>
         </Modal>
-        <Group>
-            <Text className={'inline'} sx={{
-                marginRight: '0.1rem',
-                marginLeft: '0.1rem'
-            }}>Average</Text>
-            <Select
-                className={'inline'}
-                defaultValue={Breakdowns.tps}
-                data={Object.values(Breakdowns).map(x => {
-                    return { label: x, value: x }
-                })}
-            />
-            <Text className={'inline'} sx={{
-                marginRight: '0.1rem',
-                marginLeft: '0.1rem'
-            }}>grouped by</Text>
-            <Select className={'inline'}
-                defaultValue={BreakdownPeriods.year}
-                data={Object.values(BreakdownPeriods).map(x => {
-                    return { label: x, value: x }
-                })}
-            />
-        </Group>
+
         <div>
             <ChartActions
                 showDownload
