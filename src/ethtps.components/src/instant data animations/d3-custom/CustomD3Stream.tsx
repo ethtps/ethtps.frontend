9a81fecb23a26a3db363d772c023a8c3911e03e5
Mister_Eth <89896648+Mister-Eth@users.noreply.github.com>
2023-07-17 15:13:52 +0300

    Update CustomD3Stream.tsx

--- src/ethtps.components/src/instant data animations/d3-custom/CustomD3Stream.tsx
+++ src/ethtps.components/src/instant data animations/d3-custom/CustomD3Stream.tsx
@@ -1,8 +1,9 @@
 import * as d3 from 'd3'
 import { useCallback, useMemo, useRef, useState } from "react"
-import { Axis, getD3Scale, getDataXAxisBounds, liveDataPointExtractor, makeInteractive, measure, minimalDataPointToLiveDataPoint, useGroupedDebugMeasuredEffect } from '../..'
+import { Axis, Vector2D, dataExtractor, getD3Scale, getDataXAxisBounds, liveDataPointExtractor, makeInteractive, measure, minimalDataPointToLiveDataPoint, useGroupedDebugMeasuredEffect } from '../..'
 import { IInstantDataAnimationProps } from '../../..'
 import { GenericDictionary, LiveDataAggregator, LiveDataPoint, logToOverlay } from '../../../../ethtps.data/src'
+import { transition } from '@chakra-ui/system'
 
 const aggregator = new LiveDataAggregator()
 
@@ -10,6 +11,8 @@ const aggregator = new LiveDataAggregator()
  * Junk data generator function
  */
 const f = (x: number): number => 0// Math.sin(x / 10 + Math.random())
+
+const duration = 2000
 
 export function CustomD3Stream(props: IInstantDataAnimationProps) {
     const {
@@ -56,30 +59,12 @@ export function CustomD3Stream(props: IInstantDataAnimationProps) {
             [0, innerWidth]),
         [xBounds, innerWidth])
 
-    const [max, setMax] = useState<Partial<LiveDataPoint>>()
-    const yGen = useCallback(() => d3.scaleLinear().domain([-(liveDataPointExtractor(max, dataType) ?? 0), liveDataPointExtractor(max, dataType) ?? 1]).nice().range([0, innerHeight]), [dataType, innerHeight, max])
+    const yGen = useCallback(() => d3.scaleLinear().domain([-(liveDataPointExtractor(aggregator.maxTotal, dataType) ?? 0), liveDataPointExtractor(aggregator.maxTotal, dataType) ?? 1]).nice().range([0, innerHeight]), [dataType, innerHeight, aggregator.maxTotal])
     const yAxis = yGen()
     useGroupedDebugMeasuredEffect(() => {
         if (newestData) {
             aggregator.updateMultiple(newestData)
         }
-        setMax(m => {
-            if (!m) {
-                const cpy = new LiveDataPoint()
-                cpy.tps = aggregator.maxTotal.tps
-                cpy.gps = aggregator.maxTotal.gps
-                return cpy
-            }
-            if (!m.compare!(aggregator.maxTotal)) {
-                if (m.tps !== aggregator.maxTotal.tps) {
-                    m.tps = aggregator.maxTotal.tps
-                }
-                if (m.gps !== aggregator.maxTotal.gps) {
-                    m.gps = aggregator.maxTotal.gps
-                }
-            }
-            return m
-        })
         setData((d) => {
             let dict: GenericDictionary<LiveDataPoint> = {}
             const k = [...Object.keys(aggregator.all)]
@@ -88,6 +73,7 @@ export function CustomD3Stream(props: IInstantDataAnimationProps) {
                 if (x?.data) dict = Object.assign(dict, { [k[i]]: minimalDataPointToLiveDataPoint(x.data, k[i]) })
             }
             const c = d.length < maxEntries ? [...d] : [...d.slice(maxEntries)]
+            localStorage.setItem('customd3streamdata', JSON.stringify([...c, { ...dict }]))
             return [...c, { ...dict }]
         })
     }, 'update', 'data', [newestData, aggregator, maxEntries])
@@ -114,13 +100,144 @@ export function CustomD3Stream(props: IInstantDataAnimationProps) {
         const selectArea = () => d3.select(areaRef?.current)
         selectArea().selectAll('*').remove()
         selectArea().append('path')
-            .attr('d', area(testData))
+            .attr('d', area(testData.slice(0, testData.length - 1)))
 
+        if (testData.length >= 2) {
+            const p2 = testData[testData.length - 2] //p[n-2]
+            const p1 = testData[testData.length - 1] //p[n-1]
+
+            if (!dataExtractor(p1?.y, dataType) || !dataExtractor(p2?.y, dataType)) return
+            const getCoordinates = (d: LiveDataPoint) => ({ x: xAxis(d.x!), y: yScale(liveDataPointExtractor(d, dataType)!) }) as Vector2D
+            const getInverseCoordinates = (d: LiveDataPoint) => ({ x: xAxis(d.x!), y: yScale(-liveDataPointExtractor(d, dataType)!) }) as Vector2D // y scale is not somethingmorphic - y(-x) != -y(x) because we're drawing the chart relative to the vertical midpoint
+            const p2Top = getCoordinates(p2)
+            const p1Top = getCoordinates(p1)
+            const p2Bottom = getInverseCoordinates(p2)
+            const p1Bottom = getInverseCoordinates(p1)
+            const d3interpolator = d3.interpolateNumber
+            const interpolator = {
+                top: {
+                    x: d3interpolator(p2Top.x, p1Top.x),
+                    y: d3interpolator(p2Top.y, p1Top.y)
+                },
+                bottom: {
+                    x: d3interpolator(p2Bottom.x, p1Bottom.x),
+                    y: d3interpolator(p2Bottom.y, p1Bottom.y)
+                }
+            }
+            function toPoint(v: Vector2D) {
+                return { x: v.x, y: v.y } as Vector2D
+            }
+
+            const areaPoints = {
+                top: [p2Top, p1Top].map(toPoint),
+                bottom: [p2Bottom, p1Bottom].map(toPoint)
+            }
+            const line = d3.line<Vector2D>()
+                .x((d: any) => d.x)
+                .y((d: any) => d.y)
+                .curve(d3.curveCatmullRom.alpha(0.5))
+
+            type AreaPointPair = {
+                initial: Vector2D,
+                final: Vector2D
+            }
+            // interpolate area between p2 and p_i
+            const a = d3.area<AreaPointPair>()
+                .x0((d, i) => d.initial.x)
+                .y0((d, i) => d.initial.y)
+                .x1((d, i) => d.final.x)
+                .y1((d, i) => d.final.y)
+                .curve(d3.curveCatmullRom.alpha(0.5))
+
+            logToOverlay({
+                name: 'i_top(0.5)',
+                details: `[x: ${interpolator.top.x(0.5)}, y: ${interpolator.top.y(0.5)}]`,
+                level: 'info'
+            })
+            logToOverlay({
+                name: 'i_bottom(0.5)',
+                details: `[x: ${interpolator.bottom.x(0.5)}, y: ${interpolator.bottom.y(0.5)}]`,
+                level: 'info'
+            })
+
+            selectArea().append('path')
+                .transition()
+                .duration(duration)
+                .ease(d3.easeExpIn)
+                .attrTween('fill-opacity', () => (t) => d3.interpolateNumber(0.2, 0.5)(t).toString())
+                .attrTween('d', () => (t) => a([
+                    {
+                        initial: p2Top,
+                        final: {
+                            x: interpolator.top.x(t),
+                            y: interpolator.top.y(t)
+                        } as Vector2D,
+                    },
+                    {
+                        initial: p2Bottom,
+                        final: {
+                            x: interpolator.bottom.x(t),
+                            y: interpolator.bottom.y(t)
+                        } as Vector2D,
+                    }
+                ]) ?? '')
+                .on('end', (d, i, g) => {
+
+                })
+            /*
+                        selectArea().append('path')
+                            .transition()
+                            .attr('stroke', 'red')
+                            .attr('stroke-width', 5)
+                            .attr('fill', 'red')
+                            .attr('fill-opacity', 0.5)
+                            .duration(duration)
+                            .attrTween('d', () => (t) => line([
+                                p2Top, {
+                                    x: interpolator.top.x(t),
+                                    y: interpolator.top.y(t)
+                                } as Vector2D
+                            ]) ?? '')
+                        /*
+                        .attrTween('d', () => (t) => line([
+                            p2Bottom, {
+                                x: interpolator.bottom.x(t),
+                                y: interpolator.bottom.y(t)
+                            } as Vector2D
+                        ]) ?? '')*/
+
+        }
+        //after transition
         selectArea()
             .attr('fill', 'blue')
             .attr('fill-opacity', 0.5)
             .attr('stroke', 'steelblue')
-    }, 'transform', 'data', [data, dataType, innerWidth, innerHeight, xAxis, yAxis, areaRef.current, max, pixelsPerPoint, aggregator.all, aggregator.maxTotal])
+    }, 'transform', 'data', [data, dataType, innerWidth, innerHeight, xAxis, yAxis, areaRef.current, pixelsPerPoint, aggregator.all, aggregator.maxTotal])
+    /*
+    const addJunkPoint = useCallback(() => {
+        setData(d => {
+            const target: GenericDictionary<LiveDataPoint> = {}
+            target['Polygon'] = {
+                tps: (aggregator.max.tps ?? 10) * Math.random(),
+                gps: (aggregator.max.gps ?? 10) * Math.random(),
+                x: Date.now()
+            } as LiveDataPoint
+            if (!d) return [target]
+            aggregator.update({
+                ...target['Polygon'],
+                data: target['Polygon'].y,
+                provider: 'Polygon'
+            })
+            return [...d, target]
+        })
+    }, [aggregator, setData])
+
+    useEffect(() => {
+        const id = setInterval(() => {
+            addJunkPoint()
+        }, duration * 2)
+        return () => clearInterval(id)
+    }, [addJunkPoint])*/
     return <>
         <svg
             ref={svgRef}
